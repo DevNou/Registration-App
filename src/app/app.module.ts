1225b5d4edb8833cc72482edb47d1c1dcce7e0f2
Arsenis Chatziantoniou <chatziantoniou.a@sga.gr>
2024-02-23 12:21:07 +0200

    PutAllTheSettings

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,37 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { CreateRegistrationComponent } from './create-registration/create-registration.component';
+import { RegistrationListComponent } from './registration-list/registration-list.component';
+import { UserDetailComponent } from './user-detail/user-detail.component';
+import { NgToastModule } from 'ng-angular-popup';
+import { NgConfirmModule } from 'ng-confirm-box';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateRegistrationComponent,
+    RegistrationListComponent,
+    UserDetailComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    MatToolbarModule,
+    MatIconModule,
+
+    NgToastModule,
+    NgConfirmModule
+  
   ],
-  providers: [],
+  providers: [
+    provideAnimationsAsync()
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/create-registration/create-registration.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-create-registration',
+  templateUrl: './create-registration.component.html',
+  styleUrl: './create-registration.component.scss'
+})
+export class CreateRegistrationComponent {
+
+}
